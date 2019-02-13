aec945c44105aaf2270b7fb5669a0f9b227f9f17
Mauro Lepore <maurolepore@gmail.com>
2019-02-13 12:44:13 -0500

    Support lifecycle figures

---
+++ R/copy_lifecycle_badges.R
@@ -0,0 +1,36 @@
+#' Copy into ./man/files the figures required to support lifecycle badges.
+#' 
+#' \Sexpr[results=rd, stage=render]{fgeo.template:::lifecycle("maturing")}
+#' 
+#' To support lifecycle badges you need to first run 
+#' `use_template("compat-lifecycle.R", package = "fgeo.template")`. See also
+#' <https://github.com/r-lib/rlang/blob/master/R/compat-lifecycle.R>.
+#'
+#' @param new_path Destination path of the copied figures.
+#'
+#' @return Invisible `new_path`.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # This creates a file in the root of your package. Move it to R/
+#' use_template("compat-lifecycle.R", package = "fgeo.template")
+#' # This adds the images for the badges into man/figures
+#' copy_lifecycle_figures()
+#' }
+copy_lifecycle_figures <- function(new_path = here::here("man", "figures")) {
+  if (!fs::dir_exists(new_path)) {
+    fs::dir_create(new_path)
+  }
+  figs_path <- fs::dir_ls(
+    system.file("figures", package = "fgeo.template")
+  )
+  figs_name <- fs::path_file(figs_path)
+  
+  fs::file_copy(
+    figs_path,
+    new_path = fs::path(new_path, figs_name)
+  )
+  
+  invisible(new_path)
+}
